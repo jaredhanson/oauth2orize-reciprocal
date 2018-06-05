8d7491c67fd69332511fc11d9cbb23be8490f963
Jared Hanson <jaredhanson@gmail.com>
2018-06-04 18:15:39 -0700

    Add tests.

--- test/exchange/reciprocal.test.js
+++ test/exchange/reciprocal.test.js
@@ -9,4 +9,44 @@ describe('exchange.reciprocal', function() {
     expect(reciprocal(function(){}).name).to.equal('');
   });
   
+  describe('reciprocating a code', function() {
+    var response, err;
+
+    before(function(done) {
+      function exchange(client, accessToken, code, done) {
+        if (client.id !== 'example.com') { return done(new Error('incorrect client argument')); }
+        if (accessToken !== 'sadadojsadlkjasdkljxxlkjdas') { return done(new Error('incorrect accessToken argument')); }
+        if (code !== 'hasdyubasdjahsbdkjbasd') { return done(new Error('incorrect code argument')); }
+        
+        return done();
+      }
+      
+      chai.connect.use(reciprocal(exchange))
+        .req(function(req) {
+          req.user = { id: 'example.com' };
+          req.body = {
+            code: 'hasdyubasdjahsbdkjbasd',
+            access_token: 'sadadojsadlkjasdkljxxlkjdas'
+          };
+        })
+        .end(function(res) {
+          response = res;
+          done();
+        })
+        .next(function(err) {
+          throw err;
+        })
+        .dispatch();
+    });
+    
+    it('should respond with headers', function() {
+      expect(response.getHeader('Cache-Control')).to.equal('no-store');
+      expect(response.getHeader('Pragma')).to.equal('no-cache');
+    });
+    
+    it('should respond with body', function() {
+      expect(response.body).to.be.undefined;
+    });
+  });
+  
 });
